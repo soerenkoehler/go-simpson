b3c76321ac7fdf0f5bb21f35c5c9442ef2b99e91
Sören Köhler <soerenkoehler@users.noreply.github.com>
2020-12-26 01:59:38 +0100

    debug request header

--- github/github.go
+++ github/github.go
@@ -64,8 +64,9 @@ func (context Context) APICallURL(
 		return "", err
 	}
 
-	// request.Header.Set("Content-Length", fmt.Sprint(content.Length()))
-	request.ContentLength = content.Length()
+	// request.ContentLength = content.Length()
+	request.Header.Set("Content-Length", fmt.Sprint(content.Length()))
+	request.Header.Set("Content-Type", "application/octet-stream")
 	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", context.Token))
 
 	fmt.Printf("Request: %+v\n", request) // TODO DEBUG
